49bc88619867cc6850fec718e5779b3c0902ea46
eo130 <85082832+eo130@users.noreply.github.com>
2024-04-03 12:10:11 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,28 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    int m = 0;
+    int n = 0;
+    for(int i = 0; i<myList.size(); i++){
+    String str = myList.get(i);
+    if(str.length == len){
+      m++;
+    }
+    n++;
+  }
+  return m;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
+    for(int j = 0; j<myList.size; j++){
+      String neutron = myList.get(j);
+      if(neutron.length == len)
+      myList.remove(j);
+      j--;
+    }
+    return myList;
+    }
   }
 }
